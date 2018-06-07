1fc323f730c147b538da44006d7e9b1c004b4997
Camila Maia <cmaiacd@gmail.com>
2018-06-07 20:18:41 -0300

    Add empty table cell view and add fake detail view

---
+++ ArticlesApp/ArticlesApp/ArticleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewCell.swift
+//  ArticlesApp
+//
+//  Created by Camila Maia on 07/06/18.
+//  Copyright © 2018 Camila Maia. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
