bc1fd5f5d5a7a36783b87a52779251600563d5a8
Camila Maia <cmaiacd@gmail.com>
2018-08-20 13:28:01 -0300

    Loading view

--- ArticlesApp/ArticlesApp/ArticleTableViewController.swift
+++ ArticlesApp/ArticlesApp/ArticleTableViewController.swift
@@ -1,36 +1,50 @@
 import UIKit
 
-class ArticleTableViewController: UITableViewController {
+class ArticleTableViewController: UIViewController {
+
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet var articlesTableView: UITableView!
+
     var articles = [Article]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadSampleArticles()
+        setupView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupView() {
+        articlesTableView.dataSource = self
+        articlesTableView.isHidden = true
+        loadingView.isHidden = false
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    private func loadSampleArticles() {
+        let manager = DataManager()
+        manager.fetchArticles { articles in
+            self.articles = articles
+            DispatchQueue.main.async {
+                self.loadingView.isHidden = true
+                self.articlesTableView.isHidden = false
+                self.articlesTableView.reloadData()
+            }
+        }
     }
+}
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension ArticleTableViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "ArticleTableViewCell"
-        
+
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ArticleTableViewCell else {
             fatalError("The dequeued cell is not an instance of ArticleTableViewCell.")
         }
-        
+
         let article = articles[indexPath.row]
 
         cell.titleLabel.text = article.title
@@ -38,62 +52,5 @@ class ArticleTableViewController: UITableViewController {
         cell.dateLabel.text = article.date
 
         return cell
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    //MARK: Private Methods
-    
-    private func loadSampleArticles() {
-        let manager = DataManager()
-        manager.fetchArticles(finished: { x in
-            self.articles = x
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        })
     }
 }
